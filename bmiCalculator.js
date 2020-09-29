fd592ea75056f046a227de912eddca146efc64d4
Llanz-dev <52281115+Llanz-dev@users.noreply.github.com>
2020-09-29 18:09:24 +0800

    updated

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -9,16 +9,22 @@ function btnCalculate(){
     var inputHeight = +document.getElementById("height").value;
     var completeResult = bmiCalculation(inputWeight, inputHeight);
     if(completeResult < 18.5){
-        document.getElementById("displayResult").innerText = "Your BMI is \"" + completeResult + "\"  you are underweight";
+        document.getElementById("displayResult").innerText = "Your BMI is \'" + completeResult + "\'";
+        document.getElementById("message").innerText = "you are underweight";
     } else if (completeResult >= 18.5 && completeResult <= 24.9){
-        document.getElementById("displayResult").innerText = "Your BMI is \'"+ completeResult + "\'  you have a normal weight";
+        document.getElementById("displayResult").innerText = "Your BMI is \'"+ completeResult + "\'";
+        document.getElementById("message").innerText = "you have a normal weight";
     } else if (completeResult >= 25.0 && completeResult <= 29.9){
-        document.getElementById("displayResult").innerText = "Your BMI is \"" + completeResult + "\"  you are overweight";
+        document.getElementById("displayResult").innerText = "Your BMI is \'" + completeResult + "\'";
+        document.getElementById("message").innerText = "you are overweight";
     } else if (completeResult >= 30.0 && completeResult <= 34.9){
-        document.getElementById("displayResult").innerText = "Your BMI is \"" + completeResult + "\"  you are Obesity Class 1";
+        document.getElementById("displayResult").innerText = "Your BMI is \'" + completeResult + "\'";
+        document.getElementById("message").innerText = "you are Obesity Class 1";
     } else if (completeResult >= 35.0 && completeResult <= 39.9){
-        document.getElementById("displayResult").innerText = "Your BMI is \"" + completeResult + "\"  you are Obesity Class 2";
+        document.getElementById("displayResult").innerText = "Your BMI is \'" + completeResult + "\'";
+        document.getElementById("message").innerText = "you are Obesity Class 2";
     } else {
-        document.getElementById("displayResult").innerText = "Your BMI is \"" + completeResult + "\"  you are Obesity Class 3";        
+        document.getElementById("displayResult").innerText = "Your BMI is \'" + completeResult + "\'";
+        document.getElementById("message").innerText = "you are Obesity Class 3";
     }
 }
