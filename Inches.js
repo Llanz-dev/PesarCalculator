1112f2e493e3144e6c0d8d48b9f074b69a0a5aa2
JolouTheRookie <barrientos.134038@iloilo.sti.edu.ph>
2020-10-03 18:55:33 +0800

    Fix 0 BMI Score and BMI Calculation in inch

--- Inches.js
+++ Inches.js
@@ -1,22 +1,54 @@
 /* 
 Meter was converted to inch
 Meter = 1 && Inch = 100;
+
+Lanz Based sa Google 
+1 Inches = 0.0254
 */
-function inchConverter(){
-    var Inches = +document.getElementById("Inches").value;
+
+/*function inchConverter(){
+    var Inches = document.getElementById("Inches").value;
     Inches*=100;
     return Inches;
-}
+}*/
+
 // Calculation
-function bmiCalculation(){
-    var inputKilogram = +document.getElementById("kilogram").value;
+/*function bmiCalculation(){
+    var inputKilogram = document.getElementById("kilogram").value;
     var result = inputKilogram / (inchConverter() * inchConverter());
     result = result.toFixed(1);
     return result;
+}*/
+
+
+//=========================================================//
+//==================== Jolou Code =========================//
+//=========================================================//
+
+// Meter converter Guro Lanz convert the inches nga gin input to meter
+// Lanze 1 inch == 0.0254 meters
+function meterConverter(inch){
+    let inh = inch;
+    return inh * 0.0254;
 }
+
+//Calculate BMI
+function bmiCalculation(kg, meter){
+    let result = kg / (meter ^ 2);
+    return result.toFixed(1);
+}
+
 // Display of Calculation
 function btnCalculate(){
-    var completeResult = bmiCalculation();
+    
+    // Get the value of the number inputted when clicking a button
+    let inches = document.getElementById("Inches").value;
+    let kilogram = document.getElementById("kilogram").value;
+
+    //BMI Score
+    var completeResult = bmiCalculation(kilogram, meterConverter(inches));
+  
+    //Displaying Classification of BMI
     if(completeResult < 18.5){
         document.getElementById("displayResult").innerText = "Your BMI is \'" + completeResult + "\'";
         document.getElementById("message").innerText = "you are underweight";
